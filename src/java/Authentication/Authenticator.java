b53e1d4b7f79df04ba5e845a99e2d2e351d91226
aguiarDavi <daviim0510@gmail.com>
2023-05-18 18:30:57 -0300

    add Authenticator class

---
+++ src/java/Authentication/Authenticator.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Authentication;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Authenticator {
+    private static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(password.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashedPassword = no.toString(16);
+            while (hashedPassword.length() < 32) {
+                hashedPassword = "0" + hashedPassword;
+            }
+            return hashedPassword;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static boolean authenticate(String username, String password) {
+        String hashedPassword = hashPassword(password);
+        if (username.equals("superadmin") && hashedPassword.equals(getHashedPassword())) { return true; } 
+        else { return false; }
+    }
+    
+    private static String getHashedPassword()  {
+        return "f52da651a075cbdbb965009e556c44d4";
+}
+
